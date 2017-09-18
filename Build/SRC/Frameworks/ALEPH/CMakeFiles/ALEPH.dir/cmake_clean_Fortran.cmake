4c573feb2628608ee74278ff5f84464b4a1d0c68
Dirk Hornung <knowledge@MacBook-Pro-de-Dirk.local>
2017-09-18 18:45:17 +0200

    gitignore

--- Build/SRC/Frameworks/ALEPH/CMakeFiles/ALEPH.dir/cmake_clean_Fortran.cmake
+++
@@ -1,6 +0,0 @@
-# Remove fortran modules provided by this target.
-FILE(REMOVE
-  "num_const.mod"
-  "NUM_CONST.mod"
-  "CMakeFiles/ALEPH.dir/num_const.mod.stamp"
-  )
--- Build/Test/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/knowledge/Developer/Physics/PhD/Program/Test/googletest/googletest
-# Build directory: /Users/knowledge/Developer/Physics/PhD/Program/build/Test/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
